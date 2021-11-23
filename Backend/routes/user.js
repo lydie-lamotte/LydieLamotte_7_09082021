6f85f25f3b6a2c7cf8cbf92a78f73e13cb751683
Lydie Lamotte <haid91@hotmail.fr>
2021-11-23 13:17:03 +0100

    ajout auth user

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
 const userCtrl = require('../controllers/user');
 
 
 router.post("/signup", userCtrl.signup); //enregistrer un utilisateur
 router.post("/login", userCtrl.login);  //connecter un utilisateur
-router.get("/", userCtrl.findAllUser);//récupérer tous les utilisateurs
-router.get("/:id", userCtrl.findOneUser);//récupérer un utilisateur
-router.delete("/delete/:id", userCtrl.deleteUser);//supprimer un utilisateur
-router.put("/modify/:id", userCtrl.modifyUser);//modifier un utilisateur
+router.get("/",auth, userCtrl.findAllUser);//récupérer tous les utilisateurs
+router.get("/:id",auth, userCtrl.findOneUser);//récupérer un utilisateur
+router.delete("/delete/:id",auth, userCtrl.deleteUser);//supprimer un utilisateur
+router.put("/modify/:id",auth, userCtrl.modifyUser);//modifier un utilisateur
 
 module.exports = router;
